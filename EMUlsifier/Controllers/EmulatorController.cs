a37552712c9f9830102879e4aea9ac3cb14556fe
Zane Miller <designbyzane@gmail.com>
2013-12-26 08:24:50 -0800

    Local library saving finished. Handles adding, deleting, empty library, and first start just fine. Next step is to implement game scraping. Additionally a field needs to be added to the emulators that asks as a file mask for rom extentions.

--- EMUlsifier/Controllers/EmulatorController.cs
+++ EMUlsifier/Controllers/EmulatorController.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Xml.Serialization;
-using System.Runtime.Serialization;
 using System.IO;
 
 namespace EMUlsifier
--- EMUlsifier/Models/Game.cs
+++ EMUlsifier/Models/Game.cs
@@ -6,6 +6,14 @@ namespace EMUlsifier
 	{
 
 		public string title;
+		public string genre;
+		public DateTime releaseDate;
+		public string description;
+		public string rating;
+		public string publisher;
+		public string developer;
+		public float communityRating;
+
 
 		public Game ()
 		{
